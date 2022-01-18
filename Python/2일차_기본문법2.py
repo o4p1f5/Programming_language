d2ebce339c72ad0d9ef8d0149571575f4dd32852
Minjin Oh <o4p1f5@gmail.com>
2022-01-18 14:34:27 +0900

    ...

--- Python/2일차_기본문법2.py
+++ Python/2일차_기본문법2.py
@@ -116,4 +116,21 @@ while count <= 300 :
     a = a + 1
 '''
 
-kdjfklfksn
+""" 
+# 연습문제 5-1
+"""
+
+# 연습문제 5-2
+scores = []
+while True :
+    x = int(input('성적을 입력하세요(종료 시 -1 입력): '))
+    if x == -1:
+        break
+    else:
+        scores.append(x)
+sum = 0
+for score in scores :
+    sum = sum + score
+
+avg = sum / len(scores)
+print('합계 : %d, 평균 : %.2f' %(sum, avg))
